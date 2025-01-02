430b8e286c94daad72b7d7926643b198e9440767
guoying <2473548838@qq.com>
2025-01-02 23:03:22 +0800

    修改数据库连接方式

--- src/db.rs
+++ src/db.rs
@@ -1,13 +1,25 @@
 use sqlx::{mysql::MySqlPoolOptions, MySqlPool};
 use std::env;
 use dotenv::dotenv;
+use sqlx::mysql::MySqlConnectOptions;
 
 pub async fn get_db_pool() -> MySqlPool {
     dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
+    let host = env::var("DB_HOST").expect("HOST must be set");
+    let user = env::var("DB_USER").expect("USER must be set");
+    let password = env::var("DB_PASSWORD").expect("PASSWORD must be set");
+    let database = env::var("DB_DATABASE").expect("DATABASE must be set");
+
+    // 手动构建连接参数
+    let conn_options = MySqlConnectOptions::new()
+        .host(&host)
+        .username(&user)
+        .password(&password)
+        .database(&database);
+    
     MySqlPoolOptions::new()
         .max_connections(5)
-        .connect(&database_url)
+        .connect_with(conn_options)
         .await
         .expect("Failed to create pool")
 }
